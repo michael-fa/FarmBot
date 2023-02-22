eee93708b79cf7e7a5758907af5c7a8098c8916e
michael-fa <michixx.bat@gmail.com>
2023-02-22 11:30:11 +0100

    Added global box functions

---
+++ MyFreeFarmer/Game/API/GlobalBox.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+
+namespace MyFreeFarmer.Game.API
+{
+    public static class GlobalBox
+    {
+        public static bool IsShown(Farmer game)
+        {
+            var x = Utils.FindElementIfExists(game.m_Driver, By.XPath(".//*[@id='globalbox']"));
+            if (x != null && x.Displayed) return true;
+            return false;
+        }
+
+        public static bool Close(Farmer game)
+        {
+            if (IsShown(game))
+            {
+                var x = Utils.FindElementIfExists(game.m_Driver, By.XPath(".//*[@id='globalbox_close']"));
+                if (x != null && x.Displayed) x.Click();
+                return false;
+            }
+            else return false;
+        }
+    }
+}
