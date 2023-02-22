d3a0aa12edf2e7d50e09adbe0cd2141f7a8f8487
michael-fa <michixx.bat@gmail.com>
2023-02-22 11:29:51 +0100

    Added action: SelectRackItem

---
+++ MyFreeFarmer/Game/Actions/SelectRackItem.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFreeFarmer.Game
+{
+    public static partial class Actions
+    {
+        public static bool SelectRackItem(Farmer game, int item_id)
+        {
+            if (!game.m_Info.m_LoggedIn) return false;
+            var x = game.m_Driver.FindElementIfExists(By.XPath(".//*[@id='rackitem" + item_id + "']"));
+            if (x == null || !x.Displayed) return false;
+            x.Click();
+            return true;
+        }
+    }
+}
