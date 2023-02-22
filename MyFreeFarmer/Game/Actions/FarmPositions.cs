6cb897da048e8d3b1a078cb98f4e39a3d6581c67
michael-fa <michixx.bat@gmail.com>
2023-02-22 11:31:27 +0100

    Unimportant commit

---
+++ MyFreeFarmer/Game/Actions/FarmPositions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFreeFarmer.Game
+{
+    public static partial class Actions
+    {
+        public static bool Open()
+        {
+
+        }
+
+        public static bool Close()
+        {
+
+        }
+
+        public static bool HarvestField(int fieldid)
+        {
+
+        }
+
+        public static bool WaterField(int fieldid)
+        {
+
+        }
+
+        /* TO BE WORKED ON LATER.
+        public static bool ClearField(int fieldid)
+        {
+
+        }*/
+    }
+}
---
+++ MyFreeFarmer/Game/Actions/HandleObstructingElement.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFreeFarmer.Game
+{
+    public static partial class Actions
+    {
+        public static void HandleObstructingElements(Farmer game, Exception ex)
+        {
+            Log.Warning("Action on hold because: " +  ex.Message + "\n at " + ex.Source);
+        }
+    }
+}
