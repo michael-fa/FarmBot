a1b8f944706443be23337db790165567819641d3
michael-fa <michixx.bat@gmail.com>
2023-02-24 13:23:34 +0100

    Reworked GetType to work with js and some fixes

--- MyFreeFarmer/Game/API/FarmPositions.cs
+++ MyFreeFarmer/Game/API/FarmPositions.cs
@@ -20,12 +20,10 @@ namespace MyFreeFarmer.Game.API
 
             game.m_Info.m_currentLand = landid;
 
-            int _type = 0;
             try
             {
-                _type = GetType(game, landid);
-                if (_type == 0) return false;
-                var x = Utils.FindElementIfExists(game.m_Driver, By.XPath(".//*[@id='farm" + game.m_Info.m_currentFarm.ToString() + "_pos" + landid.ToString() + "']"));
+                var x = Utils.FindElementIfExists(game.m_Driver, By.XPath(".//*[@id='farm" + game.m_Info.GetCurrentFarm().ToString() + "_pos" + landid.ToString() + "']"));
+                if (GetType(game, landid) == 0) return false;
                 if (x != null && x.Displayed) x.Click();
             }
             catch (Exception ex){
@@ -34,7 +32,7 @@ namespace MyFreeFarmer.Game.API
             finally {  }
 
             //Safety check: wait until the "container" of each position is shown.
-            if (_type == 1)
+            if (GetType(game, landid) == 1)
             {
                 game.m_JavaScript.ExecuteScript("specialZoneFieldHandler(" + landid + ");");
             }
@@ -64,44 +62,9 @@ namespace MyFreeFarmer.Game.API
             return true;
         }
 
-        public static int GetType(Farmer game, int landid) 
+        public static Int64 GetType(Farmer game, int landid) 
         {
-            //farm1_pos1 -> farm_pos_tt_name
-
-            game.m_JavaScript.ExecuteScript("farmHoverPosition(1, " + game.m_Info.m_currentFarm.ToString() + ", " + landid.ToString() + ");");
-            IDictionary <string, int> PosTypes = new Dictionary<string, int>();
-            PosTypes.Add("Acker", 1);
-            PosTypes.Add("Hühnerstall", 2);
-            PosTypes.Add("Kuhstall", 3);
-            PosTypes.Add("Schafskoppel", 4);
-            PosTypes.Add("Imkerei", 5);
-            PosTypes.Add("Bauernclub", 6);
-            PosTypes.Add("Mayo-Küche", 7);
-            PosTypes.Add("Käserei", 8);
-            PosTypes.Add("Wollspinnerei", 9);
-            PosTypes.Add("Bonbonüche", 10);
-            PosTypes.Add("Fischzucht", 11);
-            PosTypes.Add("Ziegenfarm", 12);
-            PosTypes.Add("Ölpresse", 13);
-            PosTypes.Add("Spezial manufaktur", 14); //this one may be wrong
-            PosTypes.Add("Angorastall", 15);
-            PosTypes.Add("Strickerei", 16);
-            PosTypes.Add("Zimmerei", 17);
-            PosTypes.Add("Ponyhof", 18);
-            PosTypes.Add("Fahrzeughalle", 19);
-            PosTypes.Add("Biosprit-Anlage", 20);
-            PosTypes.Add("Teeverfeinerung", 21);
-            PosTypes.Add("Bergstation", 22);
-            PosTypes.Add("Sushibar", 23);
-
-            var x = Utils.FindElementIfExists(game.m_Driver, By.XPath(".//*[@id='farm" + game.m_Info.m_currentFarm.ToString() + "_pos" + landid.ToString() + "_tt']"));
-            if (x != null && x.Displayed)
-            {
-                x = x.FindElement(By.ClassName("farm_pos_tt_name"));
-                Console.WriteLine(PosTypes[x.Text]);
-                if (x != null) return PosTypes[x.Text];
-            }
-            return 0;
+            return (Int64)game.m_JavaScript.ExecuteScript("return parseInt(farms_data['farms'][" +game.m_Info.GetCurrentFarm() + "][" + landid + "]['buildingid']);");
         }
 
         /*public static bool CultivateField(int fieldid, int item_id)
