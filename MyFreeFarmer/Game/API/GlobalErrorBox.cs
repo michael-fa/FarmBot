fb3100c640bd42fa627f125b7bd9c799030e704c
toketwo <michixx.bat@gmail.com>
2025-10-03 13:13:05 +0200

    Added functions for globalERRORboxes

---
+++ MyFreeFarmer/Game/API/GlobalErrorBox.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+
+namespace MyFreeFarmer.Game.API
+{
+    public static class GlobalErrorBox
+    {
+        public static bool m_customShown = false;
+        public static void Show(Farmer game, string content, string func = null!)
+        {
+            game.m_JavaScript.ExecuteScript($"globalerrorbox({content}, {func});");
+            m_customShown = true;
+        }
+        public static bool IsShown(Farmer game)
+        {
+            var x = Utils.FindElementIfExists(game.m_Driver, By.XPath(".//*[@id='globalerrorbox']"));
+            if (x != null && x.Displayed) return true;
+            return false;
+        }
+
+        //Untested!
+        public static bool Close(Farmer game)
+        {
+            if (IsShown(game))
+            {
+                game.m_JavaScript.ExecuteScript("hideDiv('globalerrorbox'); hideDiv('globaltransp'); $('globalerrorbox_content').innerHTML = '';");
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ MyFreeFarmer/Game/Scripting/Natives/GlobalError.cs
@@ -0,0 +1,33 @@
+﻿using AMXWrapperCore;
+using MyFreeFarmer.Game.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFreeFarmer.Game.Scripting
+{
+    public static partial class Natives
+    {
+        public static int DisplayGlobalErrorBox(AMX amx1, AMXArgumentList args1, Script caller_script, Farmer game)
+        {
+            //GlobalBox.Close(game);
+            GlobalErrorBox.Show(game, args1[0].AsString(), args1[1].AsString());
+            return 1;
+        }
+
+        public static int HideGlobalErrorBox(AMX amx1, AMXArgumentList args1, Script caller_script, Farmer game)
+        {
+            //GlobalBox.Close(game);
+            GlobalErrorBox.Close(game);
+            return 1;
+        }
+
+        public static int IsGlobalErrorBoxShown(AMX amx1, AMXArgumentList args1, Script caller_script, Farmer game)
+        {
+            if (GlobalErrorBox.IsShown(game)) return 1;
+            else return 0;
+        }
+    }
+}
